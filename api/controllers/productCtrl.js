77d7dc270729c3343a16d5d003095e6d45a78c8f
tahawy111 <amer.vib582@gmail.com>
2022-10-23 16:45:45 +0200

    half of product

--- api/controllers/productCtrl.js
+++ api/controllers/productCtrl.js
@@ -1,3 +1,32 @@
-export const create = (req, res) => {
-  console.log(req.files);
+export const create = async (req, res) => {
+  const { name, description, price, quantity, category, createdBy } = req.body;
+  console.log({ name, description, price, quantity, category, createdBy });
+  let productPictures = [];
+  if (req.files.length > 0) {
+    productPictures = req.files.map((file) => {
+      return { img: file.filename };
+    });
+  } else {
+    productPictures = [{ img: req.file.filename }];
+  }
+
+  const newProduct = new Product({
+    name,
+    slug: slugify(name),
+    price,
+    description,
+    productPictures,
+    quantity,
+    category: category ? category : [],
+    createdBy: req.user._id,
+  });
+
+  try {
+    const product = await newProduct.save();
+    res.status(201).json({ product });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Error occured creating product. Please try again!" });
+  }
 };
